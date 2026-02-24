ee5d35ce109d0ff504096a99a2d15469cc8da0ea
Karl Schriek <karl.schriek@snapcd.io>
2026-02-24 11:19:41 +0100

    bump terraform provider to 0.9.7

--- root.tf
+++ root.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snapcd = {
       source = "registry.terraform.io/schrieksoft/snapcd"
-      version = "0.9.6"      
+      version = "0.9.7"      
     }
   }
 }
